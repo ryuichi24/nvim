2003d908479b63abb8e2264873bcccaf37fa8727
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-08-24 00:02:21 +0200

    feat: change bufferline mode to tabs from buffers

--- src/lua/main/lazy/plugins/bufferline.lua
+++ src/lua/main/lazy/plugins/bufferline.lua
@@ -4,7 +4,7 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     opts = {
         options = {
-            mode = "buffers",
+            mode = "tabs",
             separator_style = "slant",
         }
     }
