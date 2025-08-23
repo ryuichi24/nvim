0cd4b0f31697c476649115ef4eeaf3feeda572e1
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-08-24 00:03:37 +0200

    feat: add code companion plugin for LLM integration

---
+++ src/lua/main/lazy/plugins/code-companion.lua
@@ -0,0 +1,39 @@
+return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    opts = function()
+        require("codecompanion").setup {
+            strategies = {
+                -- Change the default chat adapter
+                chat = {
+                    adapter = "copilot",
+                },
+                inline = {
+                    adapter = "copilot",
+                },
+                agent = {
+                    adapter = "copilot",
+                },
+            },
+            opts = {
+                -- Set debug logging
+                log_level = "DEBUG",
+            },
+
+        }
+
+        vim.keymap.set({ "n", "v" }, "<leader>oa", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+        vim.keymap.set({ "n", "v" }, "<leader>oc", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+        vim.keymap.set("v", "<leader>oi",
+            function()
+                vim.api.nvim_feedkeys(":'<,'>CodeCompanion /buffer", "c", false)
+            end, { desc = "CodeCompanion buffer on selection" })
+
+        vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+        vim.cmd([[cab cc CodeCompanion]])          -- Expand 'cc' into 'CodeCompanion' in the command line
+        vim.cmd([[cab cci CodeCompanion /buffer]]) -- Expand 'cc' into 'CodeCompanion' in the command line
+    end
+}
