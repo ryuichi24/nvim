1ab1500d628d1d602903eb76380e56d0ada2458d
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-09-11 23:47:55 +0200

    feat(nvim options): add incremental and highlight search

--- src/lua/main/core/options.lua
+++ src/lua/main/core/options.lua
@@ -37,6 +37,10 @@ vim.opt.conceallevel = 0                          -- Don't hide markup
 vim.opt.concealcursor = ""                        -- Don't hide cursor line markup
 vim.opt.lazyredraw = false                        -- Don't redraw during macros
 vim.opt.synmaxcol = 300                           -- Syntax highlighting limit
+vim.opt.incsearch = true                          -- Enable incremental search
+vim.opt.hlsearch = true                           -- Highlight search results
+
+
 
 -- File handling
 vim.opt.backup = false                            -- Don't create backup files
