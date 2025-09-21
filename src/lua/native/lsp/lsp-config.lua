145a3a978d33f4a8a927d183ecba3a8faf3848d7
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-09-22 01:10:36 +0200

    feat(lsp): add diagnostic config

--- src/lua/native/lsp/lsp-config.lua
+++ src/lua/native/lsp/lsp-config.lua
@@ -29,6 +29,33 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end,
 })
 
+--
+vim.diagnostic.config({
+    -- virtual_lines = true,
+    virtual_text = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true,
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+    },
+})
+
+
+-- LSP Configurations
 
 -- Lua
 lspconfig.lua_ls.setup({
