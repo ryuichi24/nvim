6fbe0a5f1fb3bf3b41895f25d203e405e98fe5a5
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-09-27 19:46:10 +0200

    chore(plugin): update keymap for copilot

--- src/lua/native/plugins/copilot.lua
+++ src/lua/native/plugins/copilot.lua
@@ -5,7 +5,7 @@ vim.pack.add({
 })
 
 -- GitHub Copilot Settings
-vim.api.nvim_set_keymap('i', '<M-CR>', 'copilot#Accept("<CR>")',
+vim.api.nvim_set_keymap("i", "<M-CR>", "copilot#Accept(\"<CR>\")",
     { expr = true, noremap = true, silent = true })
 
 vim.g.copilot_no_tab_map = true
@@ -16,18 +16,18 @@ vim.g.copilot_filetypes = {
 }
 
 -- CopilotChat Settings
-vim.keymap.set('n', '<leader>cc', '<cmd>CopilotChat<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>cx', '<cmd>CopilotChatClose<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>cr', '<cmd>CopilotChatReset<CR>', { noremap = true, silent = true })
-vim.keymap.set('v', '<leader>ce', '<cmd>CopilotChatExplain<CR>', { noremap = true, silent = true })
-vim.keymap.set('v', '<leader>cf', '<cmd>CopilotChatFix<CR>', { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>cc", "<cmd>CopilotChat<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>cx", "<cmd>CopilotChatClose<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>cr", "<cmd>CopilotChatReset<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>ce", "<cmd>CopilotChatExplain<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>cf", "<cmd>CopilotChatFix<CR>", { noremap = true, silent = true })
 
 
 require("CopilotChat").setup({
     model = "claude-sonnet-4", -- AI model to use
     temperature = 0.1,         -- Lower = focused, higher = creative
     window = {
-        layout = "vertical",   -- 'vertical', 'horizontal', 'float'
+        layout = "vertical",   -- "vertical", "horizontal", "float"
         width = 0.3,           -- 50% of screen width
     },
     auto_insert_mode = true,   -- Enter insert mode when opening,
