91d2c67ca1eb0a0807c909f0a2d91d4e193b6f87
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-12-22 14:32:22 +0100

    feat(keymap): add more folding keymaps

--- src/lua/native/core/keymaps.lua
+++ src/lua/native/core/keymaps.lua
@@ -34,7 +34,9 @@ vim.keymap.set('n', '<leader>yc', function()
 end, { desc = 'Copy current working directory to clipboard' })
 
 -- Folding
-vim.keymap.set("n", "S", "za", { desc = "Fold lines on cursor." })
+vim.keymap.set("n", "S",  "za", { desc = "Toggle fold under cursor" })
+vim.keymap.set("n", "SS", "zM", { desc = "Fold all lines" })
+vim.keymap.set("n", "SO", "zR", { desc = "Unfold all lines" })
 
 -- Serach
 vim.keymap.set("n", "/", function()
