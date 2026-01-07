af6dc182ceaf14326a79105694e0029392d53c29
ryuichi24 <ryuichi.nishi24@gmail.com>
2026-01-07 09:38:10 +0100

    feat(copilot): remove copilot chat plugin and cleanup some config of copilot

--- src/lua/native/plugins/copilot.lua
+++ src/lua/native/plugins/copilot.lua
@@ -1,7 +1,5 @@
 vim.pack.add({
-	{ src = "https://github.com/github/copilot.vim" },
-	-- https://copilotc-nvim.github.io/CopilotChat.nvim/#/?id=vim-plug
-	{ src = "https://github.com/CopilotC-Nvim/CopilotChat.nvim" },
+    { src = "https://github.com/github/copilot.vim" },
 })
 
 -- GitHub Copilot Settings
@@ -10,26 +8,6 @@ vim.pack.add({
 vim.g.copilot_no_tab_map = false
 
 vim.g.copilot_filetypes = {
-	["*"] = true, -- Enable for all filetypes
-	["markdown"] = true, -- Disable for markdown files
+    ["*"] = true,        -- Enable for all filetypes
+    ["markdown"] = true, -- Disable for markdown files
 }
-
--- CopilotChat Settings
-vim.keymap.set({ "n", "v" }, "<leader>cc", "<cmd>CopilotChat<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>cx", "<cmd>CopilotChatClose<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>cr", "<cmd>CopilotChatReset<CR>", { noremap = true, silent = true })
-vim.keymap.set("v", "<leader>ce", "<cmd>CopilotChatExplain<CR>", { noremap = true, silent = true })
-vim.keymap.set("v", "<leader>cf", "<cmd>CopilotChatFix<CR>", { noremap = true, silent = true })
-
-require("CopilotChat").setup({
-	model = "claude-sonnet-4", -- AI model to use
-	temperature = 0.1, -- Lower = focused, higher = creative
-	window = {
-		layout = "vertical", -- "vertical", "horizontal", "float"
-		width = 0.3, -- 50% of screen width
-	},
-	auto_insert_mode = true, -- Enter insert mode when opening,
-	mappings = {
-		reset = false,
-	},
-})
