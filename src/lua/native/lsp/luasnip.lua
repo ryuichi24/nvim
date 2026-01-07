e803a909c648c22eeca8c250e67c0476b184da49
ryuichi24 <ryuichi.nishi24@gmail.com>
2026-01-07 21:57:11 +0100

    fix(luasnip): keybinding of jums is wrong

--- src/lua/native/lsp/luasnip.lua
+++ src/lua/native/lsp/luasnip.lua
@@ -26,13 +26,13 @@ ls.config.set_config({
 })
 
 -- Keybindings --
-vim.keymap.set({ "i", "s" }, "<C-j>", function()
+vim.keymap.set({ "i", "s" }, "<A-j>", function()
     if ls.expand_or_jumpable() then
         ls.expand_or_jump()
     end
 end, { silent = true })
 
-vim.keymap.set({ "i", "s" }, "<C-k>", function()
+vim.keymap.set({ "i", "s" }, "<A-k>", function()
     if ls.jumpable(-1) then
         ls.jump(-1)
     end
