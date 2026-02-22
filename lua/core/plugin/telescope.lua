0c71fda668f30b9adadaa9f49a15750ad2d810f1
ryuichi24 <ryuichi.nishi24@gmail.com>
2026-02-22 15:16:12 +0100

    chore(plugin): cleanup telescope config

--- lua/core/plugin/telescope.lua
+++ lua/core/plugin/telescope.lua
@@ -1,102 +1,136 @@
 vim.pack.add({
-    { src = "https://github.com/nvim-telescope/telescope.nvim" }
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
 })
-
-local function quickfix_replace(opts)
-    opts = opts or {}
-
-    local prefill =
-        opts.find
-        or vim.fn.getreg("/")
-        or vim.fn.expand("<cword>")
-
-    local find = vim.fn.input("Find: ", prefill)
-    if find == "" then return end
-
-    local replace = vim.fn.input("Replace with: ")
-    if replace == "" then return end
-
-    local flags = opts.flags or "g"
-
-    -- Run replace + write
-    vim.cmd(string.format(
-        "cfdo %%s/\\V%s/%s/%s | update",
-        vim.fn.escape(find, "/"),
-        vim.fn.escape(replace, "/"),
-        flags
-    ))
-
-    -- Close edited buffers safely
-    for _, bufnr in ipairs(vim.fn.getqflist({ items = 0 }).items) do
-        if vim.api.nvim_buf_is_loaded(bufnr.bufnr) then
-            pcall(vim.cmd, "bd " .. bufnr.bufnr)
-        end
-    end
-
-    -- Close quickfix window safely
-    local qf_win = vim.fn.getqflist({ winid = 0 }).winid
-    if qf_win ~= 0 and vim.api.nvim_list_wins()[1] ~= qf_win then
-        pcall(vim.api.nvim_win_close, qf_win, true)
-    end
-end
 
 local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
 
-local function telescope_qf_replace(prompt_bufnr)
-    local picker = action_state.get_current_picker(prompt_bufnr)
-    if not picker then return end
+require("telescope").setup({
+	defaults = {
+		path_display = { "smart" },
+		-- default mappings: https://github1s.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/mappings.lua#L135-L232
+		mappings = {
+			i = {
+				["<LeftMouse>"] = {
+					actions.mouse_click,
+					type = "action",
+					opts = { expr = true },
+				},
+				["<2-LeftMouse>"] = {
+					actions.double_mouse_click,
+					type = "action",
+					opts = { expr = true },
+				},
 
-    local query = picker:_get_prompt()
+				["<C-n>"] = actions.move_selection_next,
+				["<C-p>"] = actions.move_selection_previous,
 
-    -- Default <C-q> behavior
-    actions.send_to_qflist(prompt_bufnr)
-    actions.open_qflist(prompt_bufnr)
+				["<C-c>"] = actions.close,
 
-    vim.schedule(function()
-        quickfix_replace({
-            find = query,
-        })
-    end)
-end
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
 
-vim.keymap.set("n", "<leader>qr", quickfix_replace, {
-    desc = "Replace everything in quickfix",
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+				["<C-f>"] = actions.preview_scrolling_left,
+				["<C-k>"] = actions.preview_scrolling_right,
+
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+				["<M-f>"] = actions.results_scrolling_left,
+				["<M-k>"] = actions.results_scrolling_right,
+
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.complete_tag,
+				["<C-/>"] = actions.which_key,
+				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+				["<C-w>"] = { "<c-s-w>", type = "command" },
+				["<C-r><C-w>"] = actions.insert_original_cword,
+				["<C-r><C-a>"] = actions.insert_original_cWORD,
+				["<C-r><C-f>"] = actions.insert_original_cfile,
+				["<C-r><C-l>"] = actions.insert_original_cline,
+
+				-- disable c-j because we dont want to allow new lines #2123
+				["<C-j>"] = actions.nop,
+			},
+			n = {
+				["<LeftMouse>"] = {
+					actions.mouse_click,
+					type = "action",
+					opts = { expr = true },
+				},
+				["<2-LeftMouse>"] = {
+					actions.double_mouse_click,
+					type = "action",
+					opts = { expr = true },
+				},
+
+				["<esc>"] = actions.close,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+
+				-- TODO: This would be weird if we switch the ordering.
+				["j"] = actions.move_selection_next,
+				["k"] = actions.move_selection_previous,
+				["H"] = actions.move_to_top,
+				["M"] = actions.move_to_middle,
+				["L"] = actions.move_to_bottom,
+
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
+				["gg"] = actions.move_to_top,
+				["G"] = actions.move_to_bottom,
+
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+				["<C-f>"] = actions.preview_scrolling_left,
+				["<C-k>"] = actions.preview_scrolling_right,
+
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+				["<M-f>"] = actions.results_scrolling_left,
+				["<M-k>"] = actions.results_scrolling_right,
+
+				["?"] = actions.which_key,
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			-- themes: https://github1s.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/themes.lua#L108
+			theme = "ivy",
+			-- theme = "cursor",
+			-- theme = "dropdown",
+			hidden = true,
+			find_command = { "rg", "--files", "--hidden", "--glob", "!.git" },
+		},
+	},
 })
-
-
-require("telescope").setup {
-    defaults = {
-        path_display = { "smart" },
-        mappings = {
-            i = {
-                ["<C-r>"] = telescope_qf_replace,
-            },
-            n = {
-                ["<C-r>"] = telescope_qf_replace,
-            },
-        },
-    },
-    pickers = {
-        find_files = {
-            theme = "ivy",
-            hidden = true,
-            find_command = { "rg", "--files", "--hidden", "--glob", "!.git" }
-        }
-    }
-}
 
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" })
 vim.keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Find recent files" })
 vim.keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in cwd" })
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Find buffers" })
-vim.keymap.set("n", "<leader>fc", function()
-        require("telescope.builtin").find_files {
-            cwd = vim.fn.stdpath("config")
-        }
-    end,
-    { desc = "Find files in neovim config dir" })
-
 vim.keymap.set("n", "<leader>fm", function()
-    require("telescope.builtin").marks()
+	require("telescope.builtin").marks()
 end, { desc = "Find marks (Telescope)" })
+
+vim.keymap.set("n", "<leader>fc", function()
+	require("telescope.builtin").find_files({
+		cwd = vim.fn.stdpath("config"),
+	})
+end, { desc = "Find files in neovim config dir" })
