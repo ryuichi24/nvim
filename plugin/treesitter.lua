0c77d1aa8d0078ad09c0890bc61dbbda72bed099
ryuichi24 <ryuichi.nishi24@gmail.com>
2026-03-29 23:48:17 +0200

    feat(plugin): migrate treesitter to main branch

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -2,54 +2,31 @@ vim.pack.add({
 	{
 		name = "nvim-treesitter",
 		src = "https://github.com/nvim-treesitter/nvim-treesitter",
-		version = "master",
+		version = "main",
 	},
 })
 
--- import nvim-treesitter plugin
-local treesitter = require("nvim-treesitter.configs")
+local treesitter = require("nvim-treesitter")
 
--- configure treesitter
 treesitter.setup({
-	sync_install = false,
-	ignore_install = {},
-	modules = {},
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = { enable = true },
-	ensure_installed = {
-		"vim",
-		"vimdoc",
-		"bash",
-		"lua",
-		"go",
-		"json",
-		"javascript",
-		"typescript",
-		"tsx",
-		"html",
-		"css",
-		"c",
-		"cpp",
-		"python",
-		"markdown",
-		"markdown_inline",
-		"yaml",
-		"dockerfile",
-		"gitignore",
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "<C-space>",
-			node_incremental = "<C-space>",
-			scope_incremental = false,
-			node_decremental = "<bs>",
-		},
-	},
+	install_dir = vim.fn.stdpath("data") .. "/treesitter",
+})
+
+treesitter.install({
+	"lua",
+	"javascript",
+	"typescript",
+	"go",
+	"c",
+	"cpp",
+	"markdown",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "lua", "javascript", "typescript", "tsx" },
+	callback = function()
+		vim.treesitter.start()
+	end,
 })
 
 vim.api.nvim_create_autocmd("PackChanged", {
@@ -59,12 +36,7 @@ vim.api.nvim_create_autocmd("PackChanged", {
 		local package_name, evt_type = evt.data.spec.name, evt.data.kind
 		if evt_type == "update" and package_name == "nvim-treesitter" then
 			vim.notify("nvim-treesitter updated, running TSUpdate...", vim.log.levels.INFO)
-			local ok = pcall(vim.cmd, "TSUpdate")
-			if ok then
-				vim.notify("TSUpdate completed successfully!", vim.log.levels.INFO)
-			else
-				vim.notify("TSUpdate command not available yet, skipping", vim.log.levels.INFO)
-			end
+			vim.cmd("TSUpdate")
 		end
 	end,
 })
