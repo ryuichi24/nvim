47fe6f619ad9de83929c0fb7f191e545125f9c0d
ryuichi24 <ryuichi.nishi24@gmail.com>
2026-07-30 00:25:19 +0900

    feat: add fold option

--- lua/core/options/init.lua
+++ lua/core/options/init.lua
@@ -2,6 +2,8 @@ local logger = require("utils.logger"):new({ name = "options" })
 
 logger:debug("Loading options...")
 
+require("core.options.fold")
+
 -- plugin development env
 local plugin_path = "~/dev/personal/projects/nvim_plugins/*"
 vim.opt.runtimepath:append(plugin_path)
